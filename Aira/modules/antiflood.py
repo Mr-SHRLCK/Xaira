fc6f1bdfb9a627ad3f176a02e3579dd2971f789d
「shєrl✪cʞ」 <72887386+Mr-SHRLCK@users.noreply.github.com>
2021-02-01 22:28:37 +0530

    Update antiflood.py

--- Aira/modules/antiflood.py
+++ Aira/modules/antiflood.py
@@ -6,10 +6,10 @@ from telegram.error import BadRequest
 from telegram.ext import Filters, MessageHandler, CommandHandler, run_async
 from telegram.utils.helpers import mention_html
 
-from cinderella import dispatcher
-from cinderella.modules.helper_funcs.chat_status import is_user_admin, user_admin, can_restrict, can_delete
-from cinderella.modules.log_channel import loggable
-from cinderella.modules.sql import antiflood_sql as sql
+from Aira import dispatcher
+from Aira.modules.helper_funcs.chat_status import is_user_admin, user_admin, can_restrict, can_delete
+from Aira.modules.log_channel import loggable
+from Aira.modules.sql import antiflood_sql as sql
 
 FLOOD_GROUP = 3
 
