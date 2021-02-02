93383afe15b2962f22abb237c2435dfdf852cfab
「shєrl✪cʞ」 <72887386+Mr-SHRLCK@users.noreply.github.com>
2021-02-02 15:34:14 +0530

    Update dictionary.py

--- aira/modules/dictionary.py
+++ aira/modules/dictionary.py
@@ -4,7 +4,7 @@ import requests
 from telegram import Bot, Message, Update, ParseMode
 from telegram.ext import CommandHandler, run_async
 
-from cinderella import dispatcher
+from aira import dispatcher
 
 
 @run_async
