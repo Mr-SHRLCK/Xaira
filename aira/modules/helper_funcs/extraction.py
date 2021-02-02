0052542705c2f59cd03d1263deaa8f42aa58b5dc
「shєrl✪cʞ」 <72887386+Mr-SHRLCK@users.noreply.github.com>
2021-02-02 13:08:33 +0530

    Update extraction.py

--- aira/modules/helper_funcs/extraction.py
+++ aira/modules/helper_funcs/extraction.py
@@ -3,8 +3,8 @@ from typing import List, Optional
 from telegram import Message, MessageEntity
 from telegram.error import BadRequest
 
-from cinderella import LOGGER
-from cinderella.modules.users import get_user_id
+from aira import LOGGER
+from aira.modules.users import get_user_id
 
 
 def id_from_reply(message):
