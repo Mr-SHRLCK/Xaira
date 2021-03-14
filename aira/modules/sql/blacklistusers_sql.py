3695b4be55f6ed73c0ac5d500c282abfb41bb162
「shєrl✪cʞ」 <72887386+Mr-SHRLCK@users.noreply.github.com>
2021-03-14 13:45:17 +0530

    Update blacklistusers_sql.py

--- aira/modules/sql/blacklistusers_sql.py
+++ aira/modules/sql/blacklistusers_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText
 
-from cinderella.modules.sql import BASE, SESSION
+from aira.modules.sql import BASE, SESSION
 
 
 class BlacklistUsers(BASE):
