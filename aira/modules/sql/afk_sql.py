47876fc145aea3a3119194f5231e6c08fdc4e617
「shєrl✪cʞ」 <72887386+Mr-SHRLCK@users.noreply.github.com>
2021-03-14 16:51:18 +0530

    Update afk_sql.py

--- aira/modules/sql/afk_sql.py
+++ aira/modules/sql/afk_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, UnicodeText, Boolean, Integer
 
-from cinderella.modules.sql import BASE, SESSION
+from aira.modules.sql import BASE, SESSION
 
 
 class AFK(BASE):
