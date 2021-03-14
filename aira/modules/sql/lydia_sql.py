b6ad004ce4bf0788d1f241f0ce3b238fea221a25
「shєrl✪cʞ」 <72887386+Mr-SHRLCK@users.noreply.github.com>
2021-03-14 13:54:06 +0530

    Update lydia_sql.py

--- aira/modules/sql/lydia_sql.py
+++ aira/modules/sql/lydia_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String
 
-from cinderella.modules.sql import BASE, SESSION
+from aira.modules.sql import BASE, SESSION
 
 
 class ChatbotChats(BASE):
