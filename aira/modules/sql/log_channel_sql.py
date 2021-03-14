f183a96d008480275e383ec747b40e0d7dde775b
「shєrl✪cʞ」 <72887386+Mr-SHRLCK@users.noreply.github.com>
2021-03-14 13:54:29 +0530

    Update log_channel_sql.py

--- aira/modules/sql/log_channel_sql.py
+++ aira/modules/sql/log_channel_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, func, distinct
 
-from cinderella.modules.sql import BASE, SESSION
+from aira.modules.sql import BASE, SESSION
 
 
 class GroupLogs(BASE):
